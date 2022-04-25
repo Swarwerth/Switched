8828560231e2fecaf57ff12596ff892bc69f891e
Clément NGUYEN <clement.nguyen@epita.fr>
2022-04-25 20:18:31 +0200

    Adding lobby and life ?

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+            TakeDamage(20);
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
